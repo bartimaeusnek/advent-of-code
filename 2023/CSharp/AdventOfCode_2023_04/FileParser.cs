362e5122587a70b8be5c32c95946633c83e52462
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2023-12-04 21:07:35 +0100

    AoC, Day 4

---
+++ 2023/CSharp/AdventOfCode_2023_04/FileParser.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode_2023_04;
+
+public class FileParser
+{
+    private const string Preamble = "Game ";
+    private const string PreambleEnd = ": ";
+    private const string InBetween = "| ";
+    
+    private static int GetGameBegin(string line)
+    {
+        return line.IndexOf(PreambleEnd, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static Range GetWinningCardRange(string line)
+    {
+        return new Range(GetGameBegin(line) + PreambleEnd.Length, GetGameSectionEnd(line));
+    }
+    
+    public static Range GetDataRange(string line)
+    {
+        return new Range(GetGameSectionEnd(line) + InBetween.Length, ^0);
+    }
+
+    public static IEnumerable<int> GetNumbers(string section)
+    {
+        string[] rawNumbers = section.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        foreach (string rawNumber in rawNumbers)
+        {
+            yield return int.Parse(rawNumber);
+        }
+    }
+    
+    private static int GetGameSectionEnd(string line)
+    {
+        int next = line.IndexOf(InBetween, StringComparison.InvariantCultureIgnoreCase);
+        return next == -1 ? line.Length : next;
+    }
+    
+    public static int ParseGameNumber(string line)
+    {
+        return int.Parse(line[Preamble.Length..GetGameBegin(line)].Trim());
+    }
+    
+}
---
+++ 2023/CSharp/AdventOfCode_2023_04/Program.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode_2023_04;
+
+using var file = File.OpenText("input");
+string? line;
+int amount = 0;
+var queue = new List<int>();
+int regular = 0;
+while ((line = await file.ReadLineAsync()) != null)
+{
+    amount += Part1(line);
+    Part2(line, queue);
+    ++regular;
+}
+Console.WriteLine(amount);
+Console.WriteLine(regular + queue.Count);
+return;
+
+static int Part1(string line)
+{
+    int[] winningNumbers = FileParser.GetNumbers(line[FileParser.GetWinningCardRange(line)]).ToArray();
+    int[] data = FileParser.GetNumbers(line[FileParser.GetDataRange(line)]).ToArray();
+    int returnValue = 0;
+    foreach (int i in data)
+    {
+        if (!winningNumbers.Contains(i))
+            continue;
+        if (returnValue == 0)
+        {
+            ++returnValue;
+        }
+        else
+        {
+            returnValue *= 2;
+        }
+    }
+    return returnValue;
+}
+
+static void Part2(string line, IList<int> extras)
+{
+    int gameNo = FileParser.ParseGameNumber(line);
+    int[] winningNumbers = FileParser.GetNumbers(line[FileParser.GetWinningCardRange(line)]).ToArray();
+    int[] data = FileParser.GetNumbers(line[FileParser.GetDataRange(line)]).ToArray();
+    int toAdd = gameNo;
+    for (int i = 0; i < data.Length; i++)
+    {
+        int d = data[i];
+        if (!winningNumbers.Contains(d))
+            continue;
+        extras.Add(++toAdd);
+        
+        int count = extras.Count;
+        for (int index = 0; index < count; index++)
+        {
+            int x = extras[index];
+            if (x == gameNo)
+                extras.Add(toAdd);
+        }
+    }
+}
